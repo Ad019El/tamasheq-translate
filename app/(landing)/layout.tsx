7a6415c6636d43dd56194bfe604c77c9856eab6a
Ad019El <a.dafi@esi-sba.dz>
2023-08-18 19:14:42 +0100

    added footer

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,7 +1,14 @@
+import Footer from "@/components/footer";
+
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="h-full bg-[#111827] overflow-auto">
-      <div className="mx-auto max-w-screen-xl h-full w-full">{children}</div>
+    <main className="h-full bg-[#111827] overflow-auto ">
+      <div>
+        <div className="mx-auto max-w-screen-xl h-full w-full">{children}</div>
+      </div>
+      <div className="mx-auto max-w-screen-xl w-full">
+        <Footer />
+      </div>
     </main>
   );
 };
---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <p className="text-white h-10">© 2023 TamaLinguist. All Rights Reserved.</p>
+  );
+};
+
+export default Footer;
